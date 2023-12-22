7b6b2498651679fd45ff882233d5c5f54ada79a5
NewbieCoder45 <153586814+NewbieCoder45@users.noreply.github.com>
2023-12-21 19:51:48 -0500

    remove http server and express init

--- Operation Collab/routes/index.js
+++ Operation Collab/routes/index.js
@@ -1,16 +1,14 @@
-// app.listen() for port
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// Require express
+const express = require('express');
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
 });
 
-//require express
-var express = require('express')
-var router = express.Router();
+// Start the Express server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}/`);
+});
